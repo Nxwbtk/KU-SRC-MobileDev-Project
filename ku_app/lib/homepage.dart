9d9f21254cc24995b987734da393410141808a88
bsirikam <65420147@kmitl.ac.th>
2024-02-11 04:57:50 +0700

    Add Drawer

--- ku_app/lib/homepage.dart
+++ ku_app/lib/homepage.dart
@@ -12,24 +12,59 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SafeArea(
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text('Welcome to KU App'),
-            const SizedBox(
-              height: 20,
-            ),
-            const Text('You are logged in'),
-            ElevatedButton(
-                onPressed: () {
-                  FirebaseAuth.instance.signOut();
-                },
-                child: const Text('Logout'))
-          ],
+        appBar: AppBar(
+          title: const Text('KU App'),
+          backgroundColor: Colors.grey[100],
         ),
-      ),
-    ));
+        endDrawer: Drawer(
+          child: SafeArea(
+            child: ListView(
+              padding: EdgeInsets.zero,
+              children: [
+                Container(
+                  height: 100,
+                ),
+                ListTile(
+                  title: const Text('Home'),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                ),
+                ListTile(
+                  title: const Text('Profile'),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                ),
+                ListTile(
+                  title: const Text('Settings'),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                ),
+              ],
+
+            ),
+          ),
+        ),
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text('Welcome to KU App'),
+                const SizedBox(
+                  height: 20,
+                ),
+                const Text('You are logged in'),
+                ElevatedButton(
+                    onPressed: () {
+                      FirebaseAuth.instance.signOut();
+                    },
+                    child: const Text('Logout'))
+              ],
+            ),
+          ),
+        ));
   }
 }
