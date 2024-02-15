ef5ce5b9287c56035fdcbfef860f9733768b8377
bsirikam <65420147@kmitl.ac.th>
2024-02-15 23:51:04 +0700

    connect register to firebase

--- ku_app/lib/admin/admin-register-acc.dart
+++ ku_app/lib/admin/admin-register-acc.dart
@@ -1,3 +1,6 @@
+import 'package:awesome_select/awesome_select.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 class AdminRegisterAccount extends StatefulWidget {
@@ -15,6 +18,18 @@ class _AdminRegisterAccountState extends State<AdminRegisterAccount> {
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
   final confirmPasswordController = TextEditingController();
+  String facultyValue = "1";
+  String majorValue = "1";
+
+  List<S2Choice<String>> facultyOptions = [
+    S2Choice(value: "1", title: "คณะวิศวะกรรมศาสตร์"),
+  ];
+
+  List<S2Choice<String>> majorOptions = [
+    S2Choice(value: "E1", title: "วิศวกรรมคอมพิวเตอร์"),
+    S2Choice(value: "E2", title: "วิศวกรรมไฟฟ้า"),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,11 +42,15 @@ class _AdminRegisterAccountState extends State<AdminRegisterAccount> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
+              const Text(
+                "สร้างบัญชี",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 25.0),
                 child: TextFormField(
                   maxLength: 50,
-                  obscureText: true,
+                  obscureText: false,
                   decoration: const InputDecoration(
                       border: OutlineInputBorder(),
                       hintText: 'ชื่อจริง',
@@ -46,7 +65,7 @@ class _AdminRegisterAccountState extends State<AdminRegisterAccount> {
                 padding: const EdgeInsets.symmetric(horizontal: 25.0),
                 child: TextFormField(
                   maxLength: 50,
-                  obscureText: true,
+                  obscureText: false,
                   decoration: const InputDecoration(
                       border: OutlineInputBorder(),
                       hintText: 'นามสกุล',
@@ -59,39 +78,48 @@ class _AdminRegisterAccountState extends State<AdminRegisterAccount> {
               ),
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                child: TextFormField(
-                  maxLength: 50,
-                  obscureText: true,
-                  decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: 'คณะ',
-                      counter: SizedBox(
-                        height: 0,
-                        width: 0,
-                      )),
-                  controller: facultyController,
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey),
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                  child: SmartSelect<String>.single(
+                    title: "เลือกคณะ",
+                    selectedValue: facultyValue,
+                    choiceItems: facultyOptions,
+                    onChange: (state) {
+                      setState(() {
+                        facultyValue = state.value;
+                      });
+                    },
+                  ),
+                ),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 25.0, vertical: 10),
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey),
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                  child: SmartSelect<String>.single(
+                    title: "เลือกสาขา",
+                    selectedValue: majorValue,
+                    choiceItems: majorOptions,
+                    onChange: (state) {
+                      setState(() {
+                        majorValue = state.value;
+                      });
+                    },
+                  ),
                 ),
               ),
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 25.0),
                 child: TextFormField(
                   maxLength: 50,
-                  obscureText: true,
-                  decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: 'สาขา',
-                      counter: SizedBox(
-                        height: 0,
-                        width: 0,
-                      )),
-                  controller: majorController,
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                child: TextFormField(
-                  maxLength: 50,
-                  obscureText: true,
+                  obscureText: false,
                   decoration: const InputDecoration(
                       border: OutlineInputBorder(),
                       hintText: 'อีเมล',
@@ -137,17 +165,78 @@ class _AdminRegisterAccountState extends State<AdminRegisterAccount> {
                 child: Container(
                   width: double.infinity,
                   child: ElevatedButton(
-                    onPressed: () async {},
+                    onPressed: () async {
+                      showDialog(context: context, builder: (context) {
+                        return Center(child: CircularProgressIndicator());
+                      });
+                      try {
+                        if (passwordController.text != confirmPasswordController.text) {
+                          Navigator.pop(context);
+                          await showDialog(context: context, builder: (context) {
+                            return Center(
+                              child: AlertDialog(
+                                title: const Text("รหัสผ่านไม่ตรงกัน"),
+                                content: const Text("กรุณากรอกรหัสผ่านใหม่อีกครั้ง"),
+                                actions: [
+                                  TextButton(
+                                    onPressed: () {
+                                      Navigator.pop(context);
+                                    },
+                                    child: const Text("ตกลง"),
+                                  ),
+                                ],
+                              ),
+                            );
+                          });
+                        } else {
+                          Navigator.pop(context);
+                          await FirebaseAuth.instance.createUserWithEmailAndPassword(email: emailController.text, password: passwordController.text);
+                          await showDialog(context: context, builder: (context) {
+                            return AlertDialog(
+                              title: const Text("สร้างบัญชีสำเร็จ"),
+                              content: const Text("บัญชีของคุณถูกสร้างเรียบร้อยแล้ว"),
+                              actions: [
+                                TextButton(
+                                  onPressed: () {
+                                    Navigator.pop(context);
+                                  },
+                                  child: const Text("ตกลง"),
+                                ),
+                              ],
+                            );
+                          });
+                          Navigator.pop(context);
+                        }
+                      } on FirebaseAuthException catch (e) {
+                        await showDialog(context: context, builder: (context) {
+                          return AlertDialog(
+                            title: const Text("เกิดข้อผิดพลาด"),
+                            content: Text(e.message!),
+                            actions: [
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                                child: const Text("ตกลง"),
+                              ),
+                            ],
+                          );
+                        });
+                      }
+                    },
                     style: ElevatedButton.styleFrom(
                       backgroundColor: Colors.green[600],
                       shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(5.0),
                       ),
                     ),
-                    child: const Text("สร้างบัญชี", style: TextStyle(color: Colors.white),),
+                    child: const Text(
+                      "สร้างบัญชี",
+                      style: TextStyle(color: Colors.white),
+                    ),
                   ),
                 ),
-              )
+              ),
             ],
           ),
         ),
