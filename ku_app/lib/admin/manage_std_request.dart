1b47f78141283331aa08c0695ffacedb507929ff
bsirikam <65420147@kmitl.ac.th>
2024-03-11 21:13:44 +0700

    Add manage request page

---
+++ ku_app/lib/admin/manage_std_request.dart
@@ -0,0 +1,62 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class ManageReqPage extends StatefulWidget {
+  const ManageReqPage({super.key});
+
+  @override
+  State<ManageReqPage> createState() => _ManageReqPageState();
+}
+
+class _ManageReqPageState extends State<ManageReqPage> {
+  CollectionReference requestCollection =
+      FirebaseFirestore.instance.collection('std_request');
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "จัดการคำร้อง",
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: StreamBuilder(
+        stream: requestCollection.snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+              itemCount: snapshot.data!.docs.length,
+              itemBuilder: (context, index) {
+                DocumentSnapshot request = snapshot.data!.docs[index];
+                return Card(
+                    color: request['status'] == 0 ? Colors.teal[100] : request['status'] == -1 ? Colors.red : Colors.green,
+                    child: ListTile(
+                      // ignore: prefer_interpolation_to_compose_strings
+                      title: Text("หัวข้อ: " + request['topic'], style: const TextStyle(fontWeight: FontWeight.bold)),
+                      // ignore: prefer_interpolation_to_compose_strings
+                      subtitle: Text("รายละเอียด: " + (request['detail'].length > 20 ? request['detail'].substring(0, 20) + '...' : request['detail']),),
+                      onTap: () {
+                        // Navigator.push(
+                        //     context,
+                        //     MaterialPageRoute(
+                        //         builder: (context) => const ViewEditRequestPage(),
+                        //         settings: RouteSettings(
+                        //           arguments: request
+                        //         )
+                        //       )
+                        //     );
+                      },
+                    ),
+                  );
+              },
+            );
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
+      ),
+    );
+  }
+}
